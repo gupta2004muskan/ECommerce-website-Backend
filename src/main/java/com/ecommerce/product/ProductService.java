bb916550c9d4726b879598ba16637943fea67803
Muskan Gupta <146300664+gupta2004muskan@users.noreply.github.com>
2026-04-21 10:21:15 +0530

    Create ProductService.java for product management

---
+++ src/main/java/com/ecommerce/product/ProductService.java
@@ -0,0 +1,16 @@
+package com.ecommerce.product;
+
+public class ProductService {
+    // Methods for managing products
+    public void addProduct(Product product) {
+        // Implementation here
+    }
+
+    public void updateProduct(Product product) {
+        // Implementation here
+    }
+
+    public void deleteProduct(int productId) {
+        // Implementation here
+    }
+}
